11b1d0015429ca885cbc6075144da31f01f1275d
minkovaelena <minkovaelena@users.noreply.github.com>
2025-10-30 00:54:00 +0200

    Add integration tests

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Documentation for README.md
 
 # Update 2025-10-03 10:03
+
+# Update 2025-10-30 00:54
--- config.py
+++ config.py
@@ -4,3 +4,5 @@ Configuration settings
 
 DEBUG = True
 VERSION = "1.0.0"
+
+# Update 2025-10-30 00:54
